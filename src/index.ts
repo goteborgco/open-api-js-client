c0f2cea36de97f8c1e1097da960f42b10d0fd609
Adam Alexandersson <adam.alexandersson@consid.se>
2025-03-31 00:06:40 +0200

    Added all entities as export

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,17 @@ export { GoteborgCoApi } from './Api';
 // Essential types for basic usage
 export type { GuideFilter, EventFilter, PlaceFilter, LangFilter, SearchFilter, SortOptions } from './interfaces/common';
 
-// Note: All other types are considered internal implementation details 
+// Entities
+export { WpEntity } from './entities/WpEntity';
+export { Media } from './entities/Media';
+export { MediaSizes } from './entities/MediaSizes';
+export { Image } from './entities/Image';
+export { Location } from './entities/Location';
+export { Contact } from './entities/Contact';
+export { CurrentInTime } from './entities/CurrentInTime';
+export { Related } from './entities/Related';
+export { Translations } from './entities/Translations';
+export { Markers } from './entities/Markers';
+export { EventDate } from './entities/EventDate';
+export { Taxonomy } from './entities/Taxonomy';
+export { TaxonomyTerm } from './entities/TaxonomyTerm'; 
